c6ae8cc8fdde6e3b77ed4a43e02d63c61ce5450a
“KuumaSan” <1595549405@qq.com>
2025-04-03 14:14:25 +0800

    Add frontend

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '.')));
+
+// Serve the main HTML file for all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Charity DAO frontend running on http://localhost:${port}`);
+}); 
